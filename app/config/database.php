f8651cf6cb13a3b9a9e667b0adc610174f826d6b
thetthetaye <thethet.aye@innov8te.com.sg>
2016-11-02 14:54:19 +0800

    Update Project

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'inventor_vinmotordb',
+			'username'  => 'inventor_vinusr',
+			'password'  => '!%zcFNyLE{b4',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
